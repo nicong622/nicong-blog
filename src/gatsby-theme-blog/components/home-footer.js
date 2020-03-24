cde85371c82a720776f9ccdb5deaaa9df61dacac
liangningcong <liangningcong@gmail.com>
2020-03-24 19:54:43 +0800

    feat(home): 添加备案信息

---
+++ src/gatsby-theme-blog/components/home-footer.js
@@ -0,0 +1,35 @@
+import React, { Fragment } from "react"
+import { Styled, css } from "theme-ui"
+
+const Footer = ({ socialLinks }) => (
+  <footer
+    css={css({
+      mt: 4,
+      pt: 3,
+    })}
+  >
+    © {new Date().getFullYear()}, Powered by
+    {` `}
+    <Styled.a href="https://www.gatsbyjs.org">Gatsby</Styled.a>
+    {` `}&bull;{` `}
+    {socialLinks.map((platform, i, arr) => (
+      <Fragment key={platform.url}>
+        <Styled.a href={platform.url} target="_blank" rel="noopener noreferrer">
+          {platform.name}
+        </Styled.a>
+        {arr.length - 1 !== i && (
+          <Fragment>
+            {` `}&bull;{` `}
+          </Fragment>
+        )}
+      </Fragment>
+    ))}
+
+    <br />
+
+    <p>
+      <Styled.a href="http://www.beian.miit.gov.cn" target="_blank">粤ICP备19157804号</Styled.a>
+    </p>
+  </footer>
+)
+export default Footer
